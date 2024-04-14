77d40ce2af2a39ddf14060af2450bfbaf73d3ff4
owaiskhilji <owaiskhilji6@gmail.com>
2024-04-14 14:15:33 +0500

    3 project

--- react-series/PasswordGenerator/src/App.jsx
+++ react-series/PasswordGenerator/src/App.jsx
@@ -7,7 +7,6 @@ let [Numallow , setNumallow] = useState(false);
 let [Charallow , setCharallow] = useState(false);
 let [password , setpassword] = useState("");
 // useRef hook  
-let passwordRef = useRef(null)
 let passwordGenerator = useCallback(()=>{
   let pass = "";
   let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
@@ -20,11 +19,12 @@ let passwordGenerator = useCallback(()=>{
   setpassword(pass)
 }
 ,[length,Numallow,Charallow,setpassword])
- 
+
+let passwordRef = useRef(null)
 // useCallback hook
 let copy_clipboard = useCallback(() => {
-  // passwordRef.current?.select();
-  // passwordRef.current?.setSelectionRange(0,4);
+  passwordRef.current?.select();
+  passwordRef.current?.setSelectionRange(0,4);
   //copy clipboard syntax
   window.navigator.clipboard.writeText(password)}
 ,[password])
@@ -34,8 +34,9 @@ let copy_clipboard = useCallback(() => {
   passwordGenerator()
  },[length,Numallow,Charallow,passwordGenerator])
  return (
-  <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500">
+  <div className="w-full max-w-lg mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500 align-center">
       <h1 className='text-white text-center my-3'>Password generator</h1>
+    
     <div className="flex shadow rounded-lg overflow-hidden mb-4">
         <input
             type="text"
@@ -50,33 +51,42 @@ let copy_clipboard = useCallback(() => {
         >copy</button>
         
     </div>
-    <div className='flex text-sm gap-x-2'>
+    
+    <div className='flex text-sm gap-x-3'>
+    
       <div className='flex items-center gap-x-1'>
         <input 
        onClick={copy_clipboard()}
        type='range'
         min={8}
         max={20}
+        value={length}
+        className='cursor-pointer'
         onChange={(e)=>{setlength(e.target.value)}}
         />
           <label>Length: {length}</label>
       </div>
+    
       <div className="flex items-center gap-x-1">
       <input
           type="checkbox"
           defaultChecked={Numallow}
-      onChange={()=>{setNumallow((pre)=> !pre)}}
+          id='Numinput'
+          onChange={()=>{setNumallow((pre)=> !pre)}}
       />
       <label htmlFor="numberInput">Numbers</label>
       </div>
+    
       <div className="flex items-center gap-x-1">
           <input
               type="checkbox"
               defaultChecked={Charallow}
+              id='charinput'
               onChange={()=>{setCharallow((pre)=> !pre)}}
           />
           <label htmlFor="characterInput">Characters</label>
       </div>
+    
     </div>
 </div>
     
